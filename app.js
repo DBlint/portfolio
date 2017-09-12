d854d218c92b87d74b304b31ef136785f3e79552
Rob Bronson <robert.j.bronson@gmail.com>
2017-09-12 11:42:12 -0700

    added media query for small screen.  adjusted positioning so photo and text each stay on their half of the screen.  Still haven't put in the hamburger menu.

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function School(name, start, finish, degree) {
+  this.name = name;
+  this.startDate = start;
+  this.endDate = finish;
+  this.result = degree;
+
+  this.addSchool = function(){
+    
+  }
+}
+
+function Project(name, link, description){
+  this.name = name;
+  this.url = link;
+  this.about = description;
+
+  this.addProject = function() {
+
+  }
+}
