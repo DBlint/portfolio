4e7fbb568a2156029dc5b75714ad179b07bead48
Rob Bronson <robert.j.bronson@gmail.com>
2017-09-16 12:46:15 -0700

    re-added constructors and prototypes, adjusted handlebars functions to use constructed arrays rather than raw data.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 var schools = [];
 var projects = [];
 
-/* Stuff to handle things on the page, manipulate the DOM, etc.  */
 $('#portfolio').on('click', showProjects);
 function showProjects(){
   $('.content section').hide();
@@ -24,70 +23,39 @@ $('.hamburger').on('click', showNav);
 function showNav(){
   $('nav section').toggle(400);
 }
-// var rawProjectTemp = $('#project-template').html();
-var actualProjectTemp = Handlebars.compile($('#project-template').html());
 
-// var rawSchoolTemp = $(`#school-template`).html();
-var actualSchoolTemp = Handlebars.compile($(`#school-template`).html());
+function Project(projectObj){
+  this.name = projectObj.title;
+  this.url = projectObj.url;
+  this.about = projectObj.about;
+}
+function School(schoolObj) {
+  this.name = schoolObj.name;
+  this.startDate = schoolObj.startDate;
+  this.endDate = schoolObj.endDate;
+  this.award = schoolObj.award;
+}
 
- /* Stuff to handle the data, and append to page  */
-// function School(schoolObj) {
-//   this.name = schoolObj.name;
-//   this.startDate = schoolObj.startDate;
-//   this.endDate = schoolObj.endDate;
-//   this.award = schoolObj.award;
-// }
-//
-// function Project(projectObj){
-//   this.name = projectObj.title;
-//   this.url = projectObj.url;
-//   this.about = projectObj.about;
-// }
+Project.prototype.toHTML = function(){
+  var content = Handlebars.compile($('#project-template').html());
+  return content(this);
+}
+School.prototype.toHTML = function(){
+  var content = Handlebars.compile($('#school-template').html());
+  return content(this);
+}
 
-// School.prototype.toHTML = function(){
-//   var $newSchool = $('.template.education').clone();
-//   $newSchool.removeClass('template');
-//   $newSchool.attr('data-education', '')
-//   $newSchool.find('h2.name').text(this.name);
-//   $newSchool.find('p.startDate').text(this.startDate);
-//   $newSchool.find('p.endDate').text(this.endDate);
-//   $newSchool.find('p.award').text(this.award);
-//   return $newSchool;
-// }
+schoolObjects.forEach(function(object){
+  schools.push(new School(object));
+});
+projectObjects.forEach(function(object){
+  projects.push(new Project(object));
+})
 
-
-// Project.prototype.toHTML = function(){
-//   var $newEntry = $('.template.projects ').clone();
-//   $newEntry.removeClass('template');
-//   $newEntry.attr('data-projects', '');
-//   $newEntry.find('h2').text(this.name);
-//   $newEntry.find('.url').attr('href', this.url);
-//   $newEntry.find('.description').text(this.about);
-//   return $newEntry;
-//}
-// projectObjects.forEach(function(project){
-//   projects.push(new Project(project));
-// });
-//
-// schoolObjects.forEach(function(school){
-//   schools.push(new School(school));
-// });
-
-schoolObjects.forEach(function(school){
-  $('.content').append(actualSchoolTemp(school));
+schools.forEach(function(school){
+  $('.content').append(school.toHTML());
 });
 
-projectObjects.forEach(function(project){
-  $('.content').append(actualProjectTemp(project));
+projects.forEach(function(project){
+  $('.content').append(project.toHTML());
 });
-
-/*
-
-rawData.forEach(function(articleObject) {
-  articles.push(new Article(articleObject));
-});
-
-articles.forEach(function(article){
-  $('#articles').append(article.toHtml());
-});
-*/
