1b9048078d2d090f01d2f52a89e8d2f9ca12b424
Rob Bronson <robert.j.bronson@gmail.com>
2017-09-20 12:16:09 -0700

    finished initializing Node server to serve index.html

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const EXPRESS = require('express');
+const APP = EXPRESS();
+const PORT = 3000;
+
+APP.use(EXPRESS.static('public'));
+
+APP.get('/', function(request, response){
+  response.sendFile('index.html', {root: './public'});
+});
+
+APP.get('/', function(request, response){
+  response.sendFile('index.html', {root: './public'})
+});
+
+APP.listen(PORT, function(){
+  console.log(`Express server currently running on port ${PORT}`);
+});
