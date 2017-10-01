1bf41a333806e9864c663c2428b90a044130d08f
Rob Bronson <robert.j.bronson@gmail.com>
2017-10-01 15:52:50 -0700

    restructured files for mvc, added pages.js library to html

---
+++ public/scripts/controllers/routes.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var app = app || {};
+
+page('/', somecontroller. indexhtml);
+page('/schools', somecontroller to show schools);
+page('/projects', somecontroller to show projects);
+page('/repos', somecontroller to show repos from GH);
+page('/about', some controller to show the about me);
+page();
